3cdf7079bb93aeb9b81fc49c23f651c67d997bd4
rajatjhamnani <rajatjhamnani@gmail.com>
2024-08-11 23:31:01 +0530

    some correction

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+// });
+// server.listen(3000, () => {
+//   console.log("rajat");
+// });
+
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Rajat</h1>');
 });
-server.listen(3000, () => {
-  console.log("rajat");
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
